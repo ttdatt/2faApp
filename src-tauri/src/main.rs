a057ba905c7752980943e7b2a9b778c3bb4cc510
dat.tran <ttdat.nt@gmail.com>
2024-01-21 16:21:31 +0700

    Migrate to Tauri (#1)
    * migrate to tauri
    
    * update

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,70 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use chrono::Local;
+use std::fs::OpenOptions;
+use std::io::Write;
+use tauri::{CustomMenuItem, Manager, Menu, Submenu};
+
+fn try_write_log(
+    message: &str,
+    filename: &str,
+    append: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let exec_path = std::env::current_exe()?;
+    let exec_dir = exec_path.parent().ok_or("No parent directory")?;
+    let log_path = exec_dir.join(filename);
+
+    let mut file = OpenOptions::new()
+        .append(append)
+        .create(true)
+        .write(true)
+        .open(log_path)?;
+
+    let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.3f");
+    let log_entry = format!("{}: {}\n", timestamp, message);
+
+    file.write_all(log_entry.as_bytes())?;
+    Ok(())
+}
+
+#[tauri::command]
+fn write_log(message: &str) {
+    if let Err(e) = try_write_log(message, "log.txt", true) {
+        eprintln!("Recovered from error: {:?}", e);
+    }
+}
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    message: String,
+}
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("CmdOrControl+W");
+    let import = CustomMenuItem::new("import".to_string(), "Import").accelerator("CmdOrControl+I");
+    let submenu = Submenu::new("File", Menu::new().add_item(import).add_item(quit));
+    let menu = Menu::new().add_submenu(submenu);
+
+    write_log("----------------------------------------");
+    write_log("application started");
+    tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "import" => {
+                let _ = event.window().emit_all(
+                    "open-dialog",
+                    Payload {
+                        message: "import".into(),
+                    },
+                );
+            }
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![write_log])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,124 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import CountdownCircle from "./CountdownCircle";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
+import { DataInterface, OtpItemInterface } from "./types";
+import { getToken } from "./utils";
+import debounce from "lodash/debounce";
+import { compressToUTF16, decompressFromUTF16 } from "lz-string";
+import { invoke } from "@tauri-apps/api";
+import { readTextFile, writeTextFile } from "@tauri-apps/api/fs";
+import { resourceDir } from "@tauri-apps/api/path";
+import { listen } from "@tauri-apps/api/event";
+import { open } from "@tauri-apps/api/dialog";
+import { writeText } from "@tauri-apps/api/clipboard";
+
+const ListItem = ({ x }: { x: OtpItemInterface }) => {
+	const { toast } = useToast();
+	const [otp, setOtp] = useState<string>(getToken(x.secret));
+
+	const refreshOtp = useCallback(() => {
+		setOtp(getToken(x.secret));
+	}, [x.secret]);
+
+	return (
+		<div
+			className="flex items-center justify-between py-1 cursor-pointer pb-2"
+			onKeyDown={undefined}
+			onClick={async () => {
+				await writeText(otp);
+				toast({ description: otp });
+			}}
+		>
+			<div className="flex flex-col cursor-pointer">
+				<Label className="text-xl font-normal">{x.name}</Label>
+				<Label className="text-3xl font-normal">{otp}</Label>
+				<Label className="text-xl font-normal h-7">{x.otp.account}</Label>
+			</div>
+			<CountdownCircle onFinishStep={refreshOtp} />
+		</div>
+	);
+};
+
+function App() {
+	const originData = useRef<DataInterface>();
+	const [items, setItems] = useState<OtpItemInterface[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			await invoke("write_log", { message: "load file" });
+			const dir = await resourceDir();
+			const content = await readTextFile(`${dir}/output.bin`);
+			const data = JSON.parse(decompressFromUTF16(content));
+			originData.current = data;
+			setItems(data.services);
+			await invoke("write_log", { message: "load file end" });
+		})();
+	}, []);
+
+	useEffect(() => {
+		const unlisten = listen("open-dialog", async () => {
+			const selected = await open();
+			if (selected) {
+				const content = await readTextFile(selected as string);
+				const data = JSON.parse(content);
+				setItems(data.services);
+				originData.current = data;
+
+				const compressText = compressToUTF16(content);
+				const dir = await resourceDir();
+				await writeTextFile(`${dir}output.bin`, compressText);
+			}
+		});
+		return () => {
+			unlisten.then((f) => f());
+		};
+	}, []);
+
+	const onChangeText = useCallback(
+		debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+			const text = e.target.value;
+
+			if (!originData.current) return;
+
+			if (text?.length > 0) {
+				setItems(
+					originData.current?.services?.filter(
+						(y) =>
+							y.name.toLowerCase().includes(text.toLowerCase()) ||
+							y.otp.account?.toLowerCase().includes(text.toLowerCase()),
+					),
+				);
+			} else setItems(originData.current?.services);
+		}, 300),
+		[],
+	);
+
+	invoke("write_log", { message: "render" });
+
+	if (items.length === 0) return null;
+
+	invoke("write_log", { message: "render with result" });
+
+	return (
+		<div className="flex flex-col pt-4 px-4">
+			<Input
+				autoFocus
+				autoCorrect="off"
+				autoComplete="off"
+				type="text"
+				className="mb-4"
+				onChange={onChangeText}
+			/>
+
+			{items?.map((x) => {
+				return <ListItem x={x} key={x.name + x.otp.account} />;
+			})}
+			<Toaster />
+		</div>
+	);
+}
+
+export default App;
